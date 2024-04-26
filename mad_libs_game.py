e4b845ca3445b1e33007f320c7625c3f7a52b428
nifra-coder <nifracoder@gmail.com>
2024-04-26 22:12:02 +0530

    first commit

---
+++ mad_libs_game.py
@@ -0,0 +1,32 @@
+print("\nyour madlibs story:\n")
+
+print(f"pizza was invented by a_______,______chef named ______.to make a pizza,you need to take a lump of _________ ,and make\n"
+      "a thin,round _______,______.then you cover it with____ sauce,_________ cheese\n" 
+      "and fresh choppped _______.next you have to bake it in a very hot_________.when it is done,cut it into\n"
+      "_______,_____.some kids like ______pizza the best,but my favorite is the______pizza.if i could,i would eat pizza________times a day!\n")
+
+
+
+adjective_1:str = input("Enter a adjective:")
+nationality:str= input("Enter a nationality:")
+person:str = input("Enter a person:")
+noun_1:str= input("Enter a noun:")
+adjective_2:str = input("Enter a adjective:")
+noun_2:str = input("Enter a noun:")
+adjective_3:str = input("Enter a adjective:")
+adjective_4:str = input("Enter a adjevtive:")
+plural_noun:str= input("Enter a plural")
+noun_3:str = input("Enter a noun:")
+number= input("Enter a number:")
+shapes:str = input("Enter a shapes:")
+food:str = input("Enter a food:")
+food_1:str = input("Enter a another food:")
+number_1 =input("Enter a number:")
+print(f"pizza was invented by a"+adjective_1+nationality+"chef named "+person+".to make a pizza,you need to take a lump of "+noun_1+ ",and make\n"
+      "a thin,round "+adjective_2+noun_2+".then you cover it with" +adjective_3+" sauce,"+adjective_4+" cheese\n" 
+      "and fresh choppped "+plural_noun +".next you have to bake it in a very hot" +noun_3+".when it is done,cut it into\n"
+      +number+shapes+".some kids like"+food+"pizza the best,but my favorite is the"+food_1+"pizza.if i could,i would eat pizza "+number_1+"times a day!\n")
+
+
+
+
